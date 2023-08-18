af6b4ad759ee9d9fc645e6bae0ec6685789de0fa
unknown <bilalpt>
2023-08-18 12:31:15 +0530

    hooks'useState'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
+import { useState } from 'react';
+
 
 function App() {
+
+  const Clickboss=()=>{
+    console.log('you clicked baxter')
+  }
+  const Doubleclick=(name)=>{
+    console.log('you clicked me'+name)
+  }
+
+  const [color,setColor]=useState('green')
+
+  const Change=()=>{
+    setColor('red')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+
+      <button onClick={Clickboss}>click here</button>
+      <h1 onClick={()=> Doubleclick('baxter')}>onbdouble</h1>
+
+      <h1>shirt color {color}</h1>
+      <button onClick={Change}>change color</button>
+      
     </div>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button,Table,Accordion,Card } from 'react-bootstrap'
+
+const Header = () => {
+  return (
+    <div>
+        <h1 className='bg-success'> header</h1>
+        
+      
+    </div>
+  )
+}
+
+export default Header
