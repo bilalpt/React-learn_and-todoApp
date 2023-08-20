78fb93b9783aae8167722f0ada0e1b145c7fbc4d
unknown <bilalpt>
2023-08-20 22:48:57 +0530

    last commit 8-24-2023

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './Header'
-// import { useState } from 'react';
+import { useState } from 'react';
 import Main from './components/Main';
 
 
@@ -20,7 +20,24 @@ function App() {
 
   let Another=false;
 
+  let Keydata=[{id:1,brand:'Dell',price:20000},
+              {id:2,brand:'Lenovo',price:30000},
+              {id:3,brand:'Dell',price:20000}]
 
+
+  const [products,setuser] = useState(Keydata);
+  
+  const Keyfun=(id)=>{
+    const two=products.map((item)=>
+    item.id===id?{...item, qty: item.qty+1}:item
+
+    );
+    setuser(two); 
+
+
+
+
+  }
 
 
   return (
@@ -31,6 +48,20 @@ function App() {
       {Another ? <h1>possitive</h1> : <h2>negative</h2>}
 
       <Main one={false}/>
+      {products.map((product)=>(
+        <div>
+        <h1>{product.brand}</h1>
+        <h2>{product.brand}</h2>
+        <h3>{product.id}</h3>
+
+       <button onClick={()=>Keyfun(product.id)}>+</button>
+
+        </div>
+
+
+
+
+      ))}
 
 
 
